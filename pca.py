f01aef6828fc58c6b343b39612ff2ab1c6473d03
Jason Wu <59894205+Jasonwu617@users.noreply.github.com>
2022-05-15 22:56:57 -0400

    Add files via upload

--- pca.py
+++ pca.py
@@ -2,6 +2,6 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 def pca(array):
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=1)
     pca.fit(array)
     return(pca.components_)
